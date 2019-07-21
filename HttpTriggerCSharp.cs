2876e75b081290357f8240a4b0342f6fe4bb78c5
jsonk952 <jsonk952@gmail.com>
2019-07-21 13:41:51 +1200

    test 2

--- HttpTriggerCSharp.cs
+++ HttpTriggerCSharp.cs
@@ -12,7 +12,7 @@ namespace Company.Function
 {
     public static class HttpTriggerCSharp
     {
-        [FunctionName("HttpTriggerCSharp")]
+        [FunctionName("HttpTriggerCSharp")] 
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
